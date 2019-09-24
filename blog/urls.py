3ea3f784b04001385c52956d4c8c3321cd139621
f.i.xodjaev <farruhcool1993@gmail.com>
2019-09-24 17:34:56 +0500

    added blog appwith models, forms. Now you can add comments

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from . import views
+from django.urls import path, include
+
+urlpatterns = [
+    path('', views.blog_index, name='blog_index'),
+    path('<int:pk>/', views.blog_detail, name='blog_detail'),
+    path('<category>/', views.blog_category, name='blog_category'),
+
+
+]
+
